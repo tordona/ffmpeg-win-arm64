7969d9b28dcea623050a5674b8542d5677992fb7
Terence Ordona <terence.ordona@gmail.com>
2026-03-20 17:17:14 -0400

    fix: change major_version to target_version with minor version value

--- .github/scripts/config_ffmpeg.sh
+++ .github/scripts/config_ffmpeg.sh
@@ -1,8 +1,8 @@
 generic_options=$(jq -r '.options.generic | map("--\(.status)-\(.option)") | join(" ")' ../config/options.json)
-ffmpeg_lib_options=$(jq -r --arg version "$major_version" '[.options.standard[], .options.essentials[] | .[] | select(.version <= $version) | "--enable-" + .option] | join(" ")' ../config/options.json)
+ffmpeg_lib_options=$(jq -r --arg version "$target_version" '[.options.standard[], .options.essentials[] | .[] | select(.version <= $version) | "--enable-" + .option] | join(" ")' ../config/options.json)
 if [[ "${build_type}" == "full" ]]; then
   ffmpeg_lib_options+=" "
-  ffmpeg_lib_options+=$(jq -r --arg version "$major_version" '[.options.full[] | .[] | select(.version <= $version) | "--enable-" + .option] | join(" ")' ../config/options.json)
+  ffmpeg_lib_options+=$(jq -r --arg version "$target_version" '[.options.full[] | .[] | select(.version <= $version) | "--enable-" + .option] | join(" ")' ../config/options.json)
 fi
 command="./configure ${build_config} ${generic_options} ${ffmpeg_lib_options}"
 echo "📢"
