c07d6da92d88f15117a2cfbc3e363214cf1a5a6e
Terence Ordona <terence.ordona@gmail.com>
2026-07-13 18:55:42 -0400

    build: build: add support max version support daily build exclusion

--- .github/scripts/config_ffmpeg.sh
+++ .github/scripts/config_ffmpeg.sh
@@ -1,7 +1,37 @@
+is_daily_build=$([[ "$release_tag" == *"daily-autobuild"* ]] && echo "true" || echo "false")
+
 generic_options=$(jq -r '.options.generic | map("--\(.status)-\(.option)") | join(" ")' ../config/options.json)
-ffmpeg_lib_options=$(jq -r --arg version "$target_version" '[.options.standard[], .options.essentials[] | .[] | select(.version <= $version) | "--enable-" + .option] | join(" ")' ../config/options.json)
+ffmpeg_lib_options="$(jq -r \
+  --arg version "$target_version" \
+  --argjson is_daily_build "$is_daily_build" '
+  [
+    .options.standard[], .options.essentials[] 
+    | .[] 
+    | select(
+        .version <= $version and 
+        (if has("max") then $version <= .max else true end) and
+        (if $is_daily_build and has("daily") then false else true end)
+      ) 
+    | "--enable-" + .option
+  ] 
+  | join(" ")
+' ../config/options.json)"
 if [[ "${build_type}" == "full" ]]; then
   ffmpeg_lib_options+=" "
-  ffmpeg_lib_options+=$(jq -r --arg version "$target_version" '[.options.full[] | .[] | select(.version <= $version) | "--enable-" + .option] | join(" ")' ../config/options.json)
+  ffmpeg_lib_options+="$(jq -r \
+    --arg version "$target_version" \
+    --argjson is_daily_build "$is_daily_build" '
+    [
+      .options.full[] 
+      | .[] 
+      | select(
+          .version <= $version and 
+          (if has("max") then $version <= .max else true end) and
+          (if $is_daily_build and has("daily") then false else true end)
+        ) 
+      | "--enable-" + .option
+    ] 
+    | join(" ")
+  ' ../config/options.json)"
 fi
 echo "${build_config} ${generic_options} ${ffmpeg_lib_options}" | xargs ./configure
